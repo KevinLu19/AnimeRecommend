7163ddce1c89595c728377865aa51aaf838ead2a
Kevin <kevinxz01@gmail.com>
2022-02-20 18:48:44 -0600

    working on query system

--- recommend.py
+++ recommend.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2.pgen import generate_grammar
 import requests
 from mal import *
 from mal import AnimeSearch
@@ -8,26 +9,42 @@ from mal import AnimeSearch
 
 BASE_MY_ANIME_LIST_URL = "https://api.jikan.moe/v4"
 
-def get_anime_query(**kwargs):
-    # How to use Kwargs: 
-    # Ex: get_anime_query(score = 9)
-    score = kwargs.get("score", None)
-    genre = kwargs.get("genre", None)
-    order_by = kwargs.get("order_by", None)
+class AnimeRecommend:
+    def __init__(self, user_anime_id, user_genre):
+        self.anime_id = user_anime_id
+        self.anime_genre = user_genre
+        self.anime_score_threshold = 8      # Filters anime by their MAL score.
+ 
+    def request_to_API(self, **kwargs):
+        re = requests.get(BASE_MY_ANIME_LIST_URL + str(*kwargs.values()))
+        # print (kwargs)
 
-def get_anime_genre(anime_id):
-    re = requests.get(BASE_MY_ANIME_LIST_URL + str(f"/anime/{anime_id}"))   
-    results = re.json()
+        return re.json()
 
-    # print (results["data"]["genres"])
+    def get_anime_query(self):
+        # genres=22&score=8&order_by=popularity
+        query_string = f"/anime?genres={self.anime_genre}&min_score={self.anime_score_threshold}&order_by=asc"
+ 
+        result = self.request_to_API(api_query = query_string)
 
-    anime_genre_list = []
-
-    for genre in  results["data"]["genres"]:
-        anime_genre_list.append(genre["name"])
+        for item in result["data"]:
+            print (item["title"])
 
 
-    print (anime_genre_list)
+    def get_anime_genre(self):
+        # URL Layout for genre: https://api.jikan.moe/v4/anime/48736
+        full_query_command = f"/anime/{self.anime_id}"
+        results = self.request_to_API(api_query=full_query_command)
+
+        # print (results["data"]["genres"])
+
+        anime_genre_list = []
+
+        for genre in results["data"]["genres"]:
+            anime_genre_list.append(genre["name"])
+
+        print(anime_genre_list)
+
 
 def main():
     # user_requested_anime = input("Enter Anime: ")
@@ -43,14 +60,14 @@ def main():
 47 - Gourmet 
 """
     print (genre_numeric_id)
+
     # user_given_genre = input("Enter your genre using , as delimiter for multiple: ")
     user_given_genre = 22
 
     anime_id = AnimeSearch(user_requested_anime).results[0].mal_id
+    anime_reco = AnimeRecommend(anime_id, user_given_genre)
 
-    get_anime_genre(anime_id)
-
-    # 48736 = Sono Bisque doll wa koi wo suru
-    # get_anime_genre(48736)
+    # anime_reco.get_anime_genre()
+    anime_reco.get_anime_query()
     
 main()
