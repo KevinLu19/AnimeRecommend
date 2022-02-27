62b4845073c49aef12238c58684907b778e407bd
Kevin <kevinxz01@gmail.com>
2022-02-27 10:13:23 -0600

    Cleaning up.

--- recommend.py
+++ recommend.py
@@ -59,6 +59,7 @@ class AnimeRecommend:
     # This function is search a specific anime and return the genre, used for when user wants to recommend the anime they entered.
     def serach_user_entered_anime(self):
         # URL Layout for genre: https://api.jikan.moe/v4/anime/48736
+        # /anime?genres=22&score=8&order_by=popularity
         full_query_command = f"/anime/{self.anime_id}"
         results = self.request_to_API(api_query=full_query_command)
 
@@ -66,6 +67,7 @@ class AnimeRecommend:
         # print(results)
 
         anime_genre_list = []
+        anime_theme_list = []
 
         for genre in results["data"]["genres"]:
             anime_genre_list.append(genre["mal_id"])
@@ -74,65 +76,20 @@ class AnimeRecommend:
         print("Genre: ", anime_genre_list)
 
         for theme in results["data"]["themes"]:
+            anime_theme_list.append(theme["name"])
             print ("Theme: ", theme["name"])
-
+                
         for demographic in results["data"]["demographics"]:
             print ("Demographic: ", demographic["name"])
 
+        
         self.get_anime_query(anime_genre_list)
 
-    # /anime?genres=22&score=8&order_by=popularity
 
 def main():
-#     GENRE = "genre"
-#     choice_prompt = input("Enter in genre to recommend anime genre or name to recommend anime by name: ")
-
-#     # user_requested_anime = "Sono Bisque doll wa koi wo suru"
-#     genre_numeric_id = """
-# 1 - Action       | 2 - Adventure 
-# 4 - Comedy       | 7 - Mystery 
-# 8 - Drama        | 9 - Ecchi 
-# 10 - Fantasy     | 14 - Horror 
-# 22 - Romance     | 24 - Sci-Fi 
-# 30 - Sport       | 36 - Slice - of - Life 
-# 37 - Supernatural| 41 - Suspense 
-# 47 - Gourmet 
-
-#    **Note: If you want to add more than one genre, separate them using a comma(,)
-# """
-    ## Can't search by Genre since it requires anime_id in order to do so.
-    # if choice_prompt.upper() == GENRE.upper():
-    #     # user types in genre
-    #     print (genre_numeric_id)
-
-    #     user_requested_genre_id = input("Enter your genre using , as delimiter for multiple: ")
-    #     new_requested_genre_id = user_requested_genre_id.replace(" ", "")
-
-    #     # Value for user_anime_id here doesn't exist since its searching via genre. Put in an invalid id to avoid the error.
-    #     anime_reco = AnimeRecommend(user_genre = new_requested_genre_id, user_anime_id = 0)
-    #     anime_reco.serach_user_entered_anime()
-
-    # else:
-    #     user_requested_anime = input("Enter Anime: ")
-
-    #     anime_id = AnimeSearch(user_requested_anime).results[0].mal_id
-    #     # Value for user_genre here does not exist. Was throwing an error saying kwargs["user_genre"] didn't exist which is true.
-    #     anime_reco = AnimeRecommend(user_anime_id = anime_id, user_genre = 11)
-    #     anime_reco.serach_user_entered_anime()
-        
-    # user_requested_anime = input("Enter Anime: ")
-    
     user_requested_anime = input("Enter Anime: ")
     anime_id = AnimeSearch(user_requested_anime).results[0].mal_id
-    # Value for user_genre here does not exist. Was throwing an error saying kwargs["user_genre"] didn't exist which is true.
     anime_reco = AnimeRecommend(user_anime_id = anime_id, user_genre = 11)
     anime_reco.serach_user_entered_anime()
 
-    # Testing purposes.
-    # user_given_genre = 22
-
-
-    # anime_reco.get_anime_query()
-    # anime_reco.serach_user_entered_anime()
-    
 main()
